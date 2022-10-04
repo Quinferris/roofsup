4794b75c7092a049ca588a5b101475ab8f3b4cd3
Quinlan <quinferris@gmail.com>
2022-10-04 16:02:52 -0400

    nav bar v1.0

--- tutorial.py
+++ tutorial.py
@@ -2,10 +2,15 @@ from flask import Flask,redirect,url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")   #name is being passed in from the url end point 
-def home(name):
-    return render_template("index.html", content=["quin","bill","tom"])
+@app.route("/")   #name is being passed in from the url end point 
+def home():
+    return render_template("index.html")
+
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
