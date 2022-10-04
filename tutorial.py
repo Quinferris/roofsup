6885bb4bab88dac56fd1efa2deea74f59857b549
Quinlan <quinferris@gmail.com>
2022-10-04 15:31:02 -0400

    py2flask

--- tutorial.py
+++ tutorial.py
@@ -2,9 +2,9 @@ from flask import Flask,redirect,url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/<name>")   #name is being passed in from the url end point 
+def home(name):
+    return render_template("index.html", content=["quin","bill","tom"])
 
 
 if __name__ == "__main__":
